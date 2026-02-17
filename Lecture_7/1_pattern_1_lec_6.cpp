9d11b240191d193a78a8585493b99c549c0e48dd
namanarora2707 <namanarora270704@gmail.com>
2026-02-17 13:12:19 +0530

    Notes

---
+++ Lecture_7/1_pattern_1_lec_6.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        // space
+        for(int j=1;j<=2*i-2;j++){
+            cout<<"  ";
+        }
+        // star
+        for(int j=1;j<=n-i+1;j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
