3942dd1296f8ec3d84cdcf049b911ff8678920b0
namanarora2707 <namanarora270704@gmail.com>
2026-02-19 16:58:21 +0530

    Notes

---
+++ Lecture_9/3_column_asymmetric.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        //space
+        // a loop
+        for(int j=1;j<=n-i;j++){
+            cout<<"  ";
+        }
+        //star
+        // b loop
+        for(int j=1;j<=i;j++){
+            cout<<"* ";
+        }
+        // b loop
+        for(int j=2;j<=i;j++){
+            cout<<"* ";
+        }
+        // a loop
+        for(int j=1;j<=n-i;j++){
+            cout<<"  ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
