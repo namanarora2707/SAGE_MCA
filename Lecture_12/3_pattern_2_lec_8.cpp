5fdd08e123a50edf7c61ed02d0ca1c002f5dad98
namanarora2707 <namanarora270704@gmail.com>
2026-02-25 11:16:48 +0530

    Notes

---
+++ Lecture_12/3_pattern_2_lec_8.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    int m=(n+1)/2;
+    for(int i=1;i<=m;i++){
+        // left star
+        for(int j=1;j<=m-i;j++){
+            cout<<"* ";
+        }
+        //spaces
+        for(int j=1;j<=2*i;j++){
+            cout<<"  ";
+        }
+        //right star
+        for(int j=1;j<=m-i;j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    for(int i=m-1;i>=1;i--){
+        // left star
+        for(int j=1;j<=m-i;j++){
+            cout<<"* ";
+        }
+        //spaces
+        for(int j=1;j<=2*i;j++){
+            cout<<"  ";
+        }
+        //right star
+        for(int j=1;j<=m-i;j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
