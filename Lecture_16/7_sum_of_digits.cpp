fb02bdfd5083a48e1642e7968f9cfb0b607c8152
namanarora2707 <namanarora270704@gmail.com>
2026-03-12 18:23:02 +0530

    Notes

---
+++ Lecture_16/7_sum_of_digits.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    int sum=0;
+    while(n!=0){
+        int k=n%10;
+        sum=sum+k;
+        n=n/10;
+    }
+    cout<<sum<<endl;
+    return 0;
+}
---
+++ Lecture_18/Issue.md
@@ -0,0 +1,2 @@
+No light 
+doubts and revision class
